396414f64c3aa053f240f0b67c818f78ab179aa4
ender019 <gilkr125@gmail.com>
2026-04-13 17:55:57 +0300

    deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AppLayout } from '@/components/layout/AppLayout'
 import { LandingPage } from '@/pages/LandingPage'
 import { DocsPage } from '@/pages/DocsPage'
